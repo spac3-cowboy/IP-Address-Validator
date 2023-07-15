045d23234d19cd70278dfe96e88bca2257d28481
spac3-cowboy <116919147+spac3-cowboy@users.noreply.github.com>
2023-07-16 00:05:46 +0600

    runtime issue solved.

--- main.c
+++ main.c
@@ -1,130 +1,93 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <stdbool.h>
 
-int validateIPv4Part(const char *part) {
-    // Check if part contains only digits
-    for (size_t i = 0; i < strlen(part); i++) {
-        if (!isdigit(part[i])) {
-            return 0;
-        }
-    }
-
-    // Check if part is not longer than 3 digits
-    if (strlen(part) > 3) {
-        return 0;
-    }
-
-    // Check if part has leading zeros
-    if (strlen(part) > 1 && part[0] == '0') {
-        return 0;
-    }
-
-    // Convert part to integer and check if it is within the valid range
+bool validateIPv4Part(const char *part) {
     int num = atoi(part);
-    if (num < 0 || num > 255) {
-        return 0;
-    }
-
-    return 1;
+    return (num >= 0 && num <= 255 && part[0] != '0' && atoi(part) == num);
+    // Validates if the part is a valid IPv4 part:
+    // - Checks if it is in the range of 0 to 255 (inclusive)
+    // - Ensures that the part does not have leading zeros
+    // - Compares the converted integer value with the original string to handle leading zeros properly
 }
 
-int validateIPv4(const char *ipStr) {
+bool validateIPv4(const char *ipStr) {
     int dots = 0;
     const char *ptr = ipStr;
 
-    // Check if each part of the IPv4 address is valid
     while (*ptr) {
         const char *nextDot = strchr(ptr, '.');
         const char *partEnd = (nextDot == NULL) ? ptr + strlen(ptr) : nextDot;
 
-        size_t partLen = partEnd - ptr;
-        char part[partLen + 1];
-        strncpy(part, ptr, partLen);
-        part[partLen] = '\0';
+        if (!validateIPv4Part(ptr) || ptr == partEnd)
+            return false;
+        // If the part is not a valid IPv4 part or the loop has reached the end of the string without encountering a valid part, return false
 
-        if (!validateIPv4Part(part) || ptr == partEnd) {
-            return 0;
-        }
-
-        if (nextDot != NULL) {
+        if (nextDot != NULL)
             dots++;
-        }
 
-        ptr = partEnd + 1; // Move to the next part
+        ptr = partEnd + 1;
     }
 
-    // Check if the IPv4 address contains exactly 3 dots
-    if (dots != 3) {
-        return 0;
-    }
-
-    return 1;
+    return dots == 3;
+    // Returns true if there are exactly 3 dots (indicating 4 parts)
 }
 
-int validateIPv6Part(const char *part) {
-    // Check if part is a valid hexadecimal number
-    for (size_t i = 0; i < strlen(part); i++) {
-        if (!isxdigit(part[i])) {
-            return 0;
-        }
+bool validateIPv6Part(const char *part) {
+    size_t len = strlen(part);
+    if (len > 4)
+        return false;
+    // If the part has more than 4 characters, it is invalid
+
+    for (size_t i = 0; i < len; i++) {
+        char c = part[i];
+        if (!isxdigit(c))
+            return false;
+        // If any character is not a valid hexadecimal digit, return false
     }
 
-    // Check if part is not longer than 4 digits
-    if (strlen(part) > 4) {
-        return 0;
-    }
-
-    return 1;
+    return true;
+    // All characters are valid hexadecimal digits, so return true
 }
 
-int validateIPv6(const char *ipStr) {
+bool validateIPv6(const char *ipStr) {
     int colons = 0;
     const char *ptr = ipStr;
 
-    // Check if each part of the IPv6 address is valid
     while (*ptr) {
         const char *nextColon = strchr(ptr, ':');
         const char *partEnd = (nextColon == NULL) ? ptr + strlen(ptr) : nextColon;
 
-        size_t partLen = partEnd - ptr;
-        char part[partLen + 1];
-        strncpy(part, ptr, partLen);
-        part[partLen] = '\0';
+        if (!validateIPv6Part(ptr) || ptr == partEnd)
+            return false;
+        // If the part is not a valid IPv6 part or the loop has reached the end of the string without encountering a valid part, return false
 
-        if (!validateIPv6Part(part) || ptr == partEnd) {
-            return 0;
-        }
-
-        if (nextColon != NULL) {
+        if (nextColon != NULL)
             colons++;
-        }
 
-        ptr = partEnd + 1; // Move to the next part
+        ptr = partEnd + 1;
     }
 
-    // Check if the IPv6 address contains between 2 and 7 colons
-    if (colons < 2 || colons > 7) {
-        return 0;
-    }
-
-    return 1;
+    return colons >= 2 && colons <= 7;
+    // Returns true if there are between 2 and 7 colons (indicating 3 to 8 parts)
 }
 
 int main() {
     char ipStr[40];
 
     printf("Enter an IP address: ");
-    scanf("%39s", ipStr);
+    fgets(ipStr, sizeof(ipStr), stdin);
+    ipStr[strcspn(ipStr, "\n")] = '\0';
+    // Read input from the user and remove the trailing newline character
 
-    if (validateIPv4(ipStr)) {
+    if (validateIPv4(ipStr))
         printf("Valid IPv4 address: %s\n", ipStr);
-    } else if (validateIPv6(ipStr)) {
+    else if (validateIPv6(ipStr))
         printf("Valid IPv6 address: %s\n", ipStr);
-    } else {
+    else
         printf("Invalid IP address: %s\n", ipStr);
-    }
+    // Validate the input and print the appropriate message
 
     return 0;
 }
